55117d7d56d9e3401b806a6c871ea8ba4ddf77ed
徐鹏飞 <250881478@qq.com>
2021-04-29 20:26:40 +0800

    +++

--- src/index.js
+++ src/index.js
@@ -22,15 +22,5 @@ const plugin = {
   install
 }
 
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin)
-}
-
 export default plugin
 export { BaseDialog }
